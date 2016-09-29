d9efaaa51b95f4107bb1b29ed3749c75ffa1493a
ITESM CAMPUS TAMPICO <Administrador@graphic-design11.local>
2016-09-29 17:02:52 -0500

    added map

--- ViewControllerMap.swift
+++ ViewControllerMap.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewControllerMap: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewControllerMap: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     @IBOutlet weak var listaSitios: UITableView!
     
 
+    var dict = [Dictionary<String,String>()]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listaSitios.delegate = self
+        listaSitios.dataSource = self
         get_json()
 
 
@@ -49,22 +52,33 @@ class ViewControllerMap: UIViewController, UITableViewDataSource, UITableViewDel
         }
     }
     
+    override func viewWillAppear(animated: Bool) {
+        if (dict.count != 0) {
+            dict.removeAll()
+            // Elementos del menú principal
+            dict.append(["name":"Plaza de Armas","Ciudad":"Tampico"])
+            dict.append(["name":"Laguna del Carpintero","Ciudad":"Tampico"])
+            dict.append(["name":"Playa Miramar","Ciudad":"Madero"])
+        }
+    }
+
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sitios.count
+        return dict.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let mapCell = tableView.dequeueReusableCellWithIdentifier("mapCell", forIndexPath: indexPath) as! MapCellController
         let row = indexPath.row
         
-        mapCell.menuOption.text = sitios[row]["name"]
-        mapCell.menuImage?.image = UIImage(named:sitios[row]["imageName"]!)
+        mapCell.mapNombre.text = dict[row]["name"]
+        mapCell.mapCiudad.text = dict[row]["ciudad"]
         
-        return cell
+        return mapCell
     }
     
     
--- appTampico/MapCellController.swift
+++ appTampico/MapCellController.swift
@@ -13,6 +13,7 @@ class MapCellController: UITableViewCell {
  
     @IBOutlet weak var mapNombre: UILabel!
     
+    @IBOutlet weak var mapCiudad: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
